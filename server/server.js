d1f1a984eeb3038f1f034cf09897a86467d6c042
David Cotto <dacotto1984@gmail.com>
2023-08-18 23:03:36 -0400

    working on signup validation

--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require ('cors');
-const http = require('http');
-const {Server} = require('socket.io');
-
 const app = express();
 const port = process.env.NODE_PORT || 3000
-const server = http.createServer(app);
 
 const corsOptions = [
     'http://localhost:5173'
@@ -23,22 +19,6 @@ const userRoutes = require('./routes/users');
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 
-const io = new Server(server, {
-    cors: corsOptions
-});
-
-io.on('connection', (socket) => {
-    console.log('A user connected via socket.io');
-
-    socket.on('user-login', () => {
-        console.log('user Logged in');
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Running Node Express on port: ${port}`)
 });
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,4 +1,15 @@
-<script setup>
+<script lang="ts" setup>
+import {computed, reactive} from "vue";
+
+const state = reactive({
+  password: null,
+  confirmPassword: null
+});
+
+const passwordMatch = computed(() => {
+  return state.password === state.confirmPassword
+});
+
 </script>
 
 <template>
@@ -7,16 +18,23 @@
     <h2>Sign up</h2>
     <form>
       <label for="first-name">First Name</label>
-      <input type="text" name="first-name"/>
+      <input type="text" id="first-name" name="firstName" pattern="[A-Za-z]{1,48}" required/>
 
       <label for="last-name">Last Name</label>
-      <input type="text" name="last-name">
+      <input type="text" id="last-name" name="lastName" pattern="[A-Za-z]{1,48}" required>
 
       <label for="email">Email</label>
-      <input type="text" name="email"/>
+      <input type="email" id="email" name="email" required/>
 
       <label for="password">Password</label>
-      <input type="password" name="password"/>
+      <input type="password" id="password" name="password" minlength="8" maxlength="128" v-model="state.password" autocomplete="new-password" required/>
+
+      <label for="confirm-password">Confirm Password</label>
+      <input type="password" id="confirm-password" name="confirmPassword" minlength="8" maxlength="128" v-model="state.confirmPassword" autocomplete="new-password" required/>
+
+      <p v-if="!passwordMatch" class="error-message">Passwords do not match</p>
+
+      <button :disabled="!passwordMatch" type="submit">Submit</button>
     </form>
   </div>
 
@@ -37,4 +55,11 @@
     grid-gap: 10px
     width: 300px
     height: max-content
+
+input:invalid
+  border: 2px solid red
+
+.error-message
+  color: red
+  font-weight: bold
 </style>
