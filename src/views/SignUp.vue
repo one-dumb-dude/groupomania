1c30fc5eaf483f0adac9b4a592ec3b3ab38eafc4
David Cotto <dacotto1984@gmail.com>
2023-08-27 11:40:46 -0400

    finished making validation function universal.  Need submit button to respect validity.

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,38 +1,59 @@
-<script setup>
+<script lang="ts" setup>
 import {computed, reactive, ref} from "vue";
 
-const state = reactive({
+type State = {
+  username: string | null;
+  usernameMinLength: number;
+  usernameMaxLength: number;
+  usernameRegExp: string;
+  usernameErrorMessage: string | null;
+  password: string | null;
+  passwordMinLength: number;
+  passwordMaxLength: number;
+  passwordErrorMessage: string | null;
+  confirmPassword: string | null;
+}
+
+const state = reactive<State>({
+  username: null,
+  usernameMinLength: 5,
+  usernameMaxLength: 15,
+  usernameRegExp: "^[a-zA-Z0-9]+$",
+  usernameErrorMessage: null,
   password: null,
-  confirmPassword: null,
-  firstNameError: null,
-  lastNameError: null,
-  emailError: null
+  passwordMinLength: 8,
+  passwordMaxLength: 128,
+  passwordErrorMessage: null,
+  confirmPassword: null
 });
 
-const firstNameInput = ref(null);
-const lastNameInput = ref(null);
-const emailInput = ref(null);
+const usernameRef = ref(null);
+const passwordRef = ref(null);
+
+const onInputChange = (inputReference, labelNameOfInput) => {
+  const validity = inputReference.validity;
+
+  const labelNameOfInputUppercase = `${labelNameOfInput.charAt(0).toUpperCase() + labelNameOfInput.slice(1)}`;
+  const statePropertyErrorMessage = `${labelNameOfInput}ErrorMessage`;
+
+  if (!validity.valid) {
+    if (validity.tooShort) {
+      state[statePropertyErrorMessage] = `${labelNameOfInputUppercase} too short`;
+    } else if (validity.tooLong) {
+      state[statePropertyErrorMessage] = `${labelNameOfInputUppercase} too long`;
+    } else if (validity.patternMismatch) {
+      state[statePropertyErrorMessage] = `${labelNameOfInputUppercase} should be alphanumeric`;
+    }
+  } else {
+    state[statePropertyErrorMessage] = null;
+  }
+};
 
 const passwordMatch = computed(() => {
   return state.password === state.confirmPassword
 });
 
-const checkInputValidity = (inputElement, key) => {
-  console.log(inputElement.validity);
-  if (!inputElement.validity.valid) {
-    if (inputElement.validity.typeMismatch && inputElement.type === 'email') {
-      state[key] = 'Invalid email format';
-    } else if (inputElement.validity.patternMismatch) {
-      state[key] = 'Pattern does not match';
-    } else if (inputElement.validity.tooShort) {
-      state[key] = 'Value is too short';
-    } else if (inputElement.validity.tooLong) {
-      state[key] = 'Value is too long';
-    }
-  } else {
-    state[key] = null;
-  }
-}
+
 </script>
 
 <template>
@@ -40,42 +61,43 @@ const checkInputValidity = (inputElement, key) => {
   <div id="signup-form">
     <h2>Sign up</h2>
     <form>
-      <label for="first-name">First Name</label>
+      <label for="username">Username</label>
       <input type="text"
-             id="first-name"
-             ref="firstNameInput"
-             name="firstName"
-             pattern="^[A-Za-z]{1,48}$"
-             minlength="1" maxlength="48"
-             @input="() => checkInputValidity(firstNameInput, 'firstNameError')"
-             required />
-      <span v-if="state.firstNameError">{{state.firstNameError}}</span>
-
-      <label for="last-name">Last Name</label>
-      <input type="text"
-             id="last-name"
-             ref="lastNameInput"
-             name="lastName"
-             pattern="[A-Za-z]{1,48}"
-             minlength="1" maxlength="48"
-             @input="() => checkInputValidity(lastNameInput, 'lastNameError')"
-             required>
-      <span v-if="state.lastNameError">{{state.lastNameError}}</span>
-
-      <label for="email">Email</label>
-      <input type="email"
-             id="email"
-             ref="emailInput"
-             name="email"
-             @input="()=> checkInputValidity(emailInput, 'emailError')"
+             ref="usernameRef"
+             id="username"
+             name="username"
+             :minlength="state.usernameMinLength"
+             :maxlength="state.usernameMaxLength"
+             :pattern="state.usernameRegExp"
+             @input="onInputChange(usernameRef, 'username')"
+             @blur="onInputChange(usernameRef, 'username')"
+             v-model="state.username"
              required/>
-      <span v-if="state.emailError">{{state.emailError}}</span>
+      <span v-if="state.usernameErrorMessage">{{ state.usernameErrorMessage }}</span>
 
       <label for="password">Password</label>
-      <input type="password" id="password" name="password" minlength="8" maxlength="128" v-model="state.password" autocomplete="new-password" required/>
+      <input type="password"
+             ref="passwordRef"
+             id="password"
+             name="password"
+             :minlength="state.passwordMinLength"
+             :maxlength="state.passwordMaxLength"
+             @input="onInputChange(passwordRef, 'password')"
+             @blur="onInputChange(passwordRef, 'password')"
+             v-model="state.password"
+             autocomplete="new-password"
+             required/>
+      <span v-if="state.passwordErrorMessage">{{ state.passwordErrorMessage }}</span>
 
       <label for="confirm-password">Confirm Password</label>
-      <input type="password" id="confirm-password" name="confirmPassword" minlength="8" maxlength="128" v-model="state.confirmPassword" autocomplete="new-password" required/>
+      <input type="password"
+             id="confirm-password"
+             name="confirmPassword"
+             :minlength="state.passwordMinLength"
+             :maxlength="state.passwordMaxLength"
+             v-model="state.confirmPassword"
+             autocomplete="new-password"
+             required/>
 
       <span v-if="!passwordMatch" class="error-message">Passwords do not match</span>
 
